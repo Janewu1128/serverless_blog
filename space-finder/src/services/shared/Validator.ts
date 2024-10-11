c7f80623f42942de3b0edb3289c62e16f194fc5e
Janewu <98897727+Janewu1128@users.noreply.github.com>
2024-10-10 18:20:25 -0700

    validator

---
+++ space-finder/src/services/shared/Validator.ts
@@ -0,0 +1,20 @@
+import { SpaceEntry } from "../model/Model";
+
+export class MissingFieldError extends Error {
+    constructor(missingField: string) {
+        super(`Value for ${missingField} expected!`)
+    }
+}
+
+//check if each field has the args or not, if not throw error
+export function validateAsSpaceEntry(arg: any){
+    if ((arg as SpaceEntry).location == undefined) {
+        throw new MissingFieldError('location')
+    }
+    if ((arg as SpaceEntry).name == undefined) {
+        throw new MissingFieldError('name')
+    }
+    if ((arg as SpaceEntry).id == undefined) {
+        throw new MissingFieldError('id')
+    }
+}
--- space-finder/test/launcher.ts
+++ space-finder/test/launcher.ts
@@ -13,9 +13,38 @@ process.env.TABLE_NAME = 'SpaceTable-026ef5527fa7'
 // } as any, {} as any);
 
 //SCAN/GET
+// handler({
+//     httpMethod: 'GET',
+//     queryStringParameters: {
+//         id: 'ce8dec38-835c-4782-97d6-69a2cf05d42e'
+//     }
+// } as any, {} as any);
+
+//UPDATE
+// handler({
+//     httpMethod: 'PUT',
+//     queryStringParameters: {
+//         id: 'ce8dec38-835c-4782-97d6-69a2cf05d42e'
+//     },
+//     body: JSON.stringify({
+//         location: 'Irvine update'
+//     })
+// } as any, {} as any);
+
+//DELETE
+// handler({
+//     httpMethod: 'DELETE',
+//     queryStringParameters: {
+//         id: 'ce8dec38-835c-4782-97d6-69a2cf05d42e'
+//     },
+// } as any, {} as any);
+
+//CHECK VALIDATOR
 handler({
-    httpMethod: 'GET',
-    queryStringParameters: {
-        id: 'ce8dec38-835c-4782-97d6-69a2cf05d42e'
-    }
-} as any, {} as any);
+    httpMethod: 'POST',
+    body: JSON.stringify({
+        location: 'Irvine updated'
+    })
+} as any, {} as any).then(result =>{
+    console.log(result)
+});
