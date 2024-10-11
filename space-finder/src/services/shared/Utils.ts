975cb5f196bed50d0af71cd96e36fc1c78094e80
Janewu <98897727+Janewu1128@users.noreply.github.com>
2024-10-11 15:45:54 -0700

    add Admin Group

--- space-finder/src/services/shared/Utils.ts
+++ space-finder/src/services/shared/Utils.ts
@@ -1,3 +1,4 @@
+import { APIGatewayProxyEvent } from "aws-lambda";
 import { JsonError } from "./Validator";
 import { randomUUID } from "crypto";
 
@@ -10,4 +11,12 @@ export function parseJSON(arg: string){
     } catch (error) {
         throw new JsonError(error.message)
     }
+}
+
+export function hasAdminGroup(event: APIGatewayProxyEvent){
+    const groups = event.requestContext.authorizer?.claims['cognito:groups'];
+    if (groups) {
+        return (groups as string).includes('admins');
+    }
+    return false;
 }
