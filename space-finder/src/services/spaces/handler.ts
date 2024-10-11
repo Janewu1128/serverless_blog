97c73d279fb3eb29ff92233827293c3a472b6461
Janewu <98897727+Janewu1128@users.noreply.github.com>
2024-10-10 18:19:50 -0700

    dynamodb

--- space-finder/src/services/spaces/handler.ts
+++ space-finder/src/services/spaces/handler.ts
@@ -2,6 +2,9 @@ import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
 import { postSpaces } from "./PostSpaces";
 import { getSpaces } from "./GetSpaces";
+import { updateSpaces } from "./UpdateSpaces";
+import { deleteSpaces} from "./DeleteSpaces";
+import { MissingFieldError } from "../shared/Validator";
 
 //outside handler can remain and be reused on further calls
 //first make connection to the databse and reuse that connection
@@ -15,16 +18,29 @@ async function handler(event: APIGatewayProxyEvent, context: Context): Promise<A
             case 'GET':
                 const getResponse = await getSpaces(event, ddbClient);
                 console.log(getResponse)
-                return getResponse
-                break;
+                return getResponse;
             case 'POST':
                 const postResponse = await postSpaces(event, ddbClient);
                 return postResponse;
+            case 'PUT':
+                const putResponse = await updateSpaces(event, ddbClient);
+                console.log(putResponse)
+                return putResponse;
+            case 'DELETE':
+                const deleteResponse = await deleteSpaces(event, ddbClient);
+                console.log(deleteResponse)
+                return deleteResponse;
             default:
                 break;
         }
     } catch (error) {
-        console.error(error);
+        // console.error(error);
+        if (error instanceof MissingFieldError){
+            return {
+                statusCode: 400,
+                body: JSON.stringify(error.message)
+            }
+        }
         return {
             statusCode: 500,
             body: JSON.stringify(error.message)
